762d24a690b1873f25cf51a361e7647057cbdf99
fathisiddiqi <service.fathisiddiqi@gmail.com>
2020-09-17 20:08:50 +0700

    add get data func

--- get.js
+++ get.js
@@ -1,21 +1,19 @@
-import * as uuid from "uuid";
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
-  const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
-    Item: {
+    Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: uuid.v1(),
-      content: data.content,
-      attachment: data.attachment,
-      createdAt: Date.now(),
+      noteId: event.pathParameters.id,
     },
   };
 
-  await dynamoDb.put(params);
+  const result = await dynamoDb.get(params);
+  if (!result.Item) {
+    throw new Error("Item not found.");
+  }
 
-  return params.Item;
+  return result.Item;
 });
